e0696d8b80c0a09fb4a0a079c8fa75aa4a3edde1
Abdul <abdulx9999@gmail.com>
2024-06-25 12:55:21 +0100

    Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,86 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import pandas as pd
+import requests
 
-"""
-# Welcome to Streamlit!
+st.title("Pokemon Explorer!")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+def get_details(poke_number):
+	url = f'https://pokeapi.co/api/v2/pokemon/{poke_number}/'
+	response = requests.get(url)
+	pokemon = response.json()
+	details = {}
+	details['name'] = pokemon['name'].title()
+	details['height'] =  pokemon['height'] * 10
+	details['weight'] = pokemon['weight']
+	details['move_count'] = len(pokemon['moves'])
+	details['cry'] = pokemon['cries']['latest']
+	details['sprite'] = pokemon['sprites']['front_default']
+	details['moves'] = [x['move']['name'] for x in pokemon['moves']]
+	return details
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+	
+pokemon_number = st.slider("Pick a pokemon",
+						   min_value=1,
+						   max_value=150
+						   )
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+details = get_details(pokemon_number)
+height_data = pd.DataFrame({'Pokemon': ['Weedle', details['name'], 'Victreebel'],
+							'Heights': [30, details['height'], 170]})
+weight_data = pd.DataFrame({'Pokemon': ['Weedle', details['name'], 'Victreebel'],
+               'Weights': [32, details['weight'], 155]})
+         
+color_lookup = {'fire': '#FF6C4C'}
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+colors = ['grey', '#FF6C4C', 'grey']
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5)) 
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+h_graph = sns.barplot(data = height_data, 
+                    x = 'Pokemon',
+					  y = 'Heights',
+					  palette = colors,
+					  ax=ax1)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+w_graph = sns.barplot(data = weight_data, 
+					  x = 'Pokemon',
+					  y = 'Weights',
+					  palette = colors,
+					  ax=ax2)
+
+ax1.set_title('Height')
+ax2.set_title('Weight')
+h_graph.set_xlabel('')
+w_graph.set_xlabel('')
+
+cola, colb = st.columns(2)
+with cola:
+	st.title(f'{details["name"]}')
+with colb:
+	picture = st.empty()
+	
+col1, col2 = st.columns(2)
+
+with col1:
+	tile = col1.container(height=120)
+	tile.subheader('Height 	:arrow_up:')
+	tile.subheader(f'{details["height"]}cm')
+	
+with col2:
+	tile = col2.container(height=120)
+	tile.subheader("Weight 	:weight_lifter:")
+	tile.subheader(f"{details['weight']}kg")
+
+picture.image(details['sprite'])
+
+st.audio(details['cry'], format="audio/wav", start_time=0, sample_rate=None)
+
+moves_markdown = '\n'.join("* **{}**".format(move) for move in details['moves'])
+
+st.write(f'Move Count: {details["move_count"]}')
+with st.expander("View Moves"):
+	st.write(moves_markdown)
+
+st.pyplot(fig)
